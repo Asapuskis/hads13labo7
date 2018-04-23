e49ab0f450d9c2065bb78e918110e744859a39c8
Asapuskis <aguayoasier@gmail.com>
2018-04-23 18:56:49 +0200

    primer commit labo7

---
+++ gestionWeb/Profesores/insertarTarea.aspx.vb
@@ -0,0 +1,20 @@
+﻿Public Class insertarTarea
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblUsuario.Text = Session("usuario")
+    End Sub
+
+    Protected Sub btnInsertarTarea_Click(sender As Object, e As EventArgs) Handles btnInsertarTarea.Click
+        Dim accesoDatosSQL = New accesoDatosSQL.accesoDatosSQL
+        Page.Validate()
+        If Page.IsValid Then
+            'insertar la tarea
+
+            Dim res As String = accesoDatosSQL.insertarTarea(txtCodigo.Text, txtDescripcion.Text, ListaAsignaturas.SelectedItem.Text, txtHestimadas.Text, True, listaTipoTareas.SelectedItem.Text)
+
+            lblInsertTarea.Text = res
+
+        End If
+    End Sub
+End Class
